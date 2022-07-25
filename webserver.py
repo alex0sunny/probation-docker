474ee84a5462f5c0b53b0b4fa593ff083045396e
alex0sunny <perekhodovalexey@gmail.com>
2022-07-25 17:37:52 +0300

    simplify webserver

--- webserver.py
+++ webserver.py
@@ -19,41 +19,19 @@ class CustomHandler(BaseHTTPRequestHandler):
             self.path = "/index.html"
 
         try:
-            # Check the file extension required and
-            # set the right mime type
-
-            sendReply = False
             if self.path.endswith(".html"):
                 mimetype = 'text/html'
-                sendReply = True
-            if self.path.endswith(".jpg"):
-                mimetype = 'image/jpg'
-                sendReply = True
-            if self.path.endswith(".gif"):
-                mimetype = 'image/gif'
-                sendReply = True
-            if self.path.endswith(".js"):
+            elif self.path.endswith(".js"):
                 mimetype = 'application/javascript'
-                sendReply = True
-            if self.path.endswith(".css"):
-                mimetype = 'text/css'
-                sendReply = True
+            else:
+                return
 
-            if sendReply:
-                # Open the static file requested and send it
-                if mimetype == 'image/jpg':
-                    f = open(curdir + sep + self.path, 'rb')
-                else:
-                    f = open(curdir + sep + self.path)
-                self.send_response(200)
-                self.send_header('Content-type', mimetype)
-                self.end_headers()
-                if mimetype == 'image/jpg':
-                    self.wfile.write(f.read())
-                else:
-                    self.wfile.write(f.read().encode())
-                f.close()
-            return
+            f = open(curdir + sep + self.path)
+            self.send_response(200)
+            self.send_header('Content-type', mimetype)
+            self.end_headers()
+            self.wfile.write(f.read().encode())
+            f.close()
 
         except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
@@ -69,7 +47,6 @@ class CustomHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         post_data = self.rfile.read(content_length).decode()
-        #print(f'path:{self.path}\nPOST:{dic}')
         path = self.path[1:]
         if path == CALC_PATH:
             self.send_response(200)
@@ -81,12 +58,9 @@ class CustomHandler(BaseHTTPRequestHandler):
 
 
 try:
-    # Create a web server and define the handler to manage the
-    # incoming request
     server = HTTPServer(('', PORT_NUMBER), CustomHandler)
     print('Started httpserver on port ', PORT_NUMBER)
 
-    # Wait forever for incoming htto requests
     server.serve_forever()
 
 except KeyboardInterrupt:
